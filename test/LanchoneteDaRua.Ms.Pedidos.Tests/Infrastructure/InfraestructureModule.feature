d730996d2fc10656c01ec23d8996863c9fb77cd6
Janduy Monroe <janduymonroe@wiz.co>
2024-01-13 18:27:28 -0300

    test: adicionado teste de injeção da infraestrutura

---
+++ test/LanchoneteDaRua.Ms.Pedidos.Tests/Infrastructure/InfraestructureModule.feature
@@ -0,0 +1,19 @@
+﻿Funcionalidade: Configuração da Infraestrutura
+Como um desenvolvedor
+Quero garantir que os serviços de infraestrutura sejam corretamente configurados
+Para que possam ser injetados e utilizados no aplicativo
+
+    Cenário: Adicionar configuração do MongoDB ao IServiceCollection
+        Dado que o IServiceCollection está configurado
+        Quando adiciono o módulo de infraestrutura
+        Então o cliente e o banco de dados do MongoDB devem estar disponíveis para injeção
+
+    Cenário: Adicionar repositórios ao IServiceCollection
+        Dado que o IServiceCollection está configurado
+        Quando adiciono o módulo de infraestrutura
+        Então os repositórios devem estar disponíveis para injeção
+
+    Cenário: Adicionar configuração do Message Bus ao IServiceCollection
+        Dado que o IServiceCollection está configurado
+        Quando adiciono o módulo de infraestrutura
+        Então os serviços do Message Bus devem estar disponíveis para injeção
---
+++ test/LanchoneteDaRua.Ms.Pedidos.Tests/Infrastructure/InfraestructureModuleSteps.cs
@@ -0,0 +1,60 @@
+﻿using Amazon.SQS;
+using LanchoneteDaRua.Ms.Pedidos.Domain.Repositories;
+using LanchoneteDaRua.Ms.Pedidos.Infrastructure;
+using LanchoneteDaRua.Ms.Pedidos.Infrastructure.MessageBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using TechTalk.SpecFlow;
+
+namespace LanchoneteDaRua.Ms.Pedidos.Tests.Infrastructure;
+
+[Binding]
+public class InfraestruturaSteps
+{
+    private IServiceCollection _services;
+    private IServiceProvider _provider;
+    private IConfiguration _configuration;
+
+    [Given(@"que o IServiceCollection está configurado")]
+    public void DadoQueOIServiceCollectionEstaConfigurado()
+    {
+        var configurationBuilder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Development.json", optional: true)
+            .AddEnvironmentVariables();
+        
+        _configuration = configurationBuilder.Build();
+        _services = new ServiceCollection();
+        _services.AddSingleton<IConfiguration>(_configuration);
+    }
+
+    [When(@"adiciono o módulo de infraestrutura")]
+    public void QuandoAdicionoAConfiguracaoDoMongoDB()
+    {
+        _services.AddInfraestructureLayer();
+        _provider = _services.BuildServiceProvider();
+    }
+
+    [Then(@"o cliente e o banco de dados do MongoDB devem estar disponíveis para injeção")]
+    public void EntaoOClienteEOBancoDeDadosDoMongoDBDevemEstarDisponiveisParaInjecao()
+    {
+        Assert.NotNull(_provider.GetService<IMongoClient>());
+        Assert.NotNull(_provider.GetService<IMongoDatabase>());
+    }
+    
+    [Then(@"os repositórios devem estar disponíveis para injeção")]
+    public void EntaoOsRepositóriosDevemEstarDisponíveisParaInjecao()
+    {
+        Assert.NotNull(_provider.GetService<IPedidoRepository>());
+    }
+    
+    [Then(@"os serviços do Message Bus devem estar disponíveis para injeção")]
+    public void EntaoOsServicosDoMessageBusDevemEstarDisponiveisParaInjecao()
+    {
+        Assert.NotNull(_provider.GetService<IMessageBusClient>());
+        Assert.NotNull(_provider.GetService<IEventProcessor>());
+        Assert.NotNull(_provider.GetService<IAmazonSQS>());
+    }
+
+}
