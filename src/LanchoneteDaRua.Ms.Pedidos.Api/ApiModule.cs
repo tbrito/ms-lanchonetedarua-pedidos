0b35e28ec33ac7157f55284a67c635ced0745b91
Janduy Monroe <janduymonroe@wiz.co>
2024-01-29 10:38:19 -0300

    fix: adjust some points

--- src/LanchoneteDaRua.Ms.Pedidos.Api/ApiModule.cs
+++ src/LanchoneteDaRua.Ms.Pedidos.Api/ApiModule.cs
@@ -1,5 +1,7 @@
-﻿namespace LanchoneteDaRua.Ms.Pedidos.Api;
+﻿using System.Diagnostics.CodeAnalysis;
 
+namespace LanchoneteDaRua.Ms.Pedidos.Api;
+[ExcludeFromCodeCoverage]
 public static class ApiModule
 {
     public static IServiceCollection AddApiLayer(this IServiceCollection services)
